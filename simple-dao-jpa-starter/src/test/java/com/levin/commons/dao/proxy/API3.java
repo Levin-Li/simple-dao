370c81d17ab9ea17e88f8b7fefc7b3fe334d976c
llw <vince2li@gmail.com>
2020-10-12 11:01:16 +0800

    修复 hibernate 更新语句执行后，查询会使用 session 缓存中的旧数据问题。

--- simple-dao-jpa-starter/src/test/java/com/levin/commons/dao/proxy/API3.java
+++ simple-dao-jpa-starter/src/test/java/com/levin/commons/dao/proxy/API3.java
@@ -1,5 +1,10 @@
 package com.levin.commons.dao.proxy;
 
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
 public @interface API3 {
 
 }
