962da4fc8b7ea0173263ed6201783df807685813
llw <vince2li@gmail.com>
2021-01-17 14:22:18 +0800

    增加忽略的文件

--- simple-dao-core/src/main/java/com/levin/commons/dao/Version.java
+++ simple-dao-core/src/main/java/com/levin/commons/dao/Version.java
@@ -5,7 +5,7 @@ package com.levin.commons.dao;
  */
 public final class Version {
 
-    public static final String VERSION_CODE = "2.2.0";
+    public static final String VERSION_CODE = "2.2.12-SNAPSHOT";
 
 
 }
