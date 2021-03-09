db14c848a0d0d2ba040abc8dbed58a4fa4f60b51
llw <vince2li@gmail.com>
2021-03-09 18:21:31 +0800

    增加实例代码

---
+++ simple-dao-code-gen-example/src/main/java/com/levin/commons/dao/codegen/example/entities/Group.java
@@ -0,0 +1,59 @@
+package com.levin.commons.dao.codegen.example.entities;
+
+import com.levin.commons.dao.domain.StatefulObject;
+import com.levin.commons.dao.domain.support.AbstractTreeObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldNameConstants;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+/**
+ * 示例代码
+ * <p>
+ * Created by simple-dao-code-gen on Tue Mar 02 10:13:56 CST 2021.
+ */
+@Entity(name = TableOption.PREFIX + "exam_groups")
+@Data
+@Accessors(chain = true)
+@FieldNameConstants
+public class Group
+        extends AbstractTreeObject<Long, Group>
+        implements StatefulObject<String> {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Schema(description = "状态")
+    String state;
+
+    @Schema(description = "类别")
+    String category;
+
+
+    public Group(String name, Long parentId) {
+        super(name, parentId);
+    }
+
+    public Group() {
+    }
+
+    public Group(Long id, String name) {
+        super(name, null);
+        this.id = id;
+    }
+
+    public Group(String name) {
+        super(name, null);
+    }
+
+
+    @Override
+    public String toString() {
+        return name + "[" + state + "]";
+    }
+}
---
+++ simple-dao-code-gen-example/src/main/java/com/levin/commons/dao/codegen/example/entities/User.java
@@ -0,0 +1,52 @@
+package com.levin.commons.dao.codegen.example.entities;
+
+import com.levin.commons.dao.domain.StatefulObject;
+import com.levin.commons.dao.domain.support.AbstractNamedEntityObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldNameConstants;
+
+import javax.persistence.*;
+
+/**
+ * 示例代码
+ * <p>
+ * Created by simple-dao-code-gen on Tue Mar 02 10:13:56 CST 2021.
+ */
+@Entity(name =TableOption.PREFIX +  "exam_users")
+@Data
+@Accessors(chain = true)
+@ToString(exclude = "group")
+@FieldNameConstants
+public class User
+        extends AbstractNamedEntityObject<Long>
+        implements StatefulObject<String> {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Schema(description = "状态")
+    @Column
+    String state;
+
+    @Schema(description = "区域")
+    @Column
+    String area;
+
+    @Schema(description = "组织")
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id")
+    Group group;
+
+    @Schema(description = "职业")
+    @Column
+    String job;
+
+    @Schema(description = "分数")
+    Integer score;
+
+
+}
