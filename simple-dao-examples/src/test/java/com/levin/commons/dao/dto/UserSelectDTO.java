2d85b591403c2d8496f23902281b29e84281d916
llw <vince2li@gmail.com>
2021-03-11 13:01:54 +0800

    修复测试用例

--- simple-dao-examples/src/test/java/com/levin/commons/dao/dto/UserSelectDTO.java
+++ simple-dao-examples/src/test/java/com/levin/commons/dao/dto/UserSelectDTO.java
@@ -27,10 +27,6 @@ public class UserSelectDTO extends UserDTO {
 //    @Select("score")
     UserStatDTO selectSubQueryDTO = new UserStatDTO();
 
-//    @Ignore
-    @Select(value =   "select 3000 from xxx.tab t where u.id = ${:name1}")
-    Map param = new HashMap();
-
     //子查询，并使用命名参数，命名参数从Map变量中取
     @NotExists(paramExpr = "select '${_name}' from jpa_dao_test_User t where u.id = t.id and t.score > ${:minScore} and t.name like ${groupName}")
 //            int minScore =5;
