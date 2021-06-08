3d8dcd194f8a481692b9cb19661b7198ab4f79cf
llw <vince2li@gmail.com>
2021-06-08 14:22:30 +0800

    增加CASE支持

---
+++ simple-dao-annotations/src/main/java/com/levin/commons/dao/Case.java
@@ -0,0 +1,105 @@
+package com.levin.commons.dao;
+
+import lombok.Data;
+import lombok.experimental.Accessors;
+import lombok.experimental.FieldNameConstants;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * SQL case
+ * <p>
+ * 1、简单 CASE
+ * 2、Case 搜索函数
+ * <p>
+ * <p>
+ * 简单 CASE 如下：
+ * CASE sex
+ * WHEN '1' THEN '男'
+ * ELSE '其他'
+ * END
+ * <p>
+ * 统计，选择常用
+ *
+ * @author llw
+ */
+@Data
+@Accessors(chain = true)
+@FieldNameConstants
+public class Case implements Serializable {
+
+    private final Map<String, String> whenList = new LinkedHashMap<>();
+
+    @Data
+    @Accessors(chain = true)
+    zhe g@FieldNameConstants
+    public class When implements Serializable {
+
+        String when;
+
+        String then;
+
+        @Override
+        public String toString() {
+            return " WHEN " + when + " THEN " + when;
+        }
+    }
+
+
+    /**
+     * 字段名/列名
+     *
+     * @return
+     */
+    String column;
+
+    /**
+     * @return
+     */
+    public Case when(String when, String then) {
+
+        whenList.put(when, then);
+
+        return this;
+    }
+
+    /**
+     * else 表达式
+     *
+     * @return
+     */
+    String elseExpr;
+
+    @Override
+    public String toString() {
+
+        StringBuilder ql = new StringBuilder();
+
+        ql.append(" CASE ");
+
+        ql.append(nullSafe(column));
+
+        whenList.forEach((k, v) -> ql.append(" WHEN ").append(k).append(" THEN ").append(v));
+
+        if (hasTxt(elseExpr)) {
+            ql.append(" ELSE ").append(elseExpr);
+        }
+
+        ql.append(" END ");
+
+        return ql.toString();
+
+    }
+
+
+    private static String nullSafe(String value) {
+        return hasTxt(value) ? "" : value;
+    }
+
+    private static boolean hasTxt(String value) {
+        return value == null || value.trim().length() == 0;
+    }
+
+}
