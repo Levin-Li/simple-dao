e34a77ce7c04bea13c868eacb94442c185a4d821
llw <vince2li@gmail.com>
2021-09-07 11:40:46 +0800

    优化文档

--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/testcase/shell/startup.sh
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/testcase/shell/startup.sh
@@ -15,8 +15,30 @@ pids=`ps -ef | grep java | grep "$shellDir" | awk '{print $2}'`
 
 if [ -z $pids ]; then
 
+   read -p "is need startup parameter?[y/n]" -t 5 needParam
+
+   param = "`date +%s`"
+
+   if [ "y" = needParam]; then
+
+      echo "请输入启动参数(先按回车再按 CTRL+C结束输入):"
+      echo "Please input startup parameter(press enter and ^C over):"
+
+      #主要用于输入密码，但不会在命令行历史记录出现
+      cat <&0 >param
+
+   fi
+
    echo "[$shellDir/$0] program startup ..."
-   nohup java -Dwork.dir=${shellDir} -server -Dloader.path=resources,biz-libs,third-libs -jar *.jar 2>&1 &
+
+   # -XX:+DisableAttachMechanism 禁止调试
+   nohup java -Dwork.dir=${shellDir} -server -Dsp=${param} -XX:+DisableAttachMechanism -Dloader.path=resources,biz-libs,third-libs -jar *.jar 2>&1 &
+
+   sleep 5s
+
+   echo "#param:$$" > param
+
+   rm -fr param
 
 else
 
