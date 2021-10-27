600d5f2e6de1384b48d47b2393209860c2f81287
llw <vince2li@gmail.com>
2021-10-27 16:25:08 +0800

    优化模板

--- simple-dao-core/src/main/java/com/levin/commons/dao/domain/OrderableObject.java
+++ simple-dao-core/src/main/java/com/levin/commons/dao/domain/OrderableObject.java
@@ -4,7 +4,7 @@ package com.levin.commons.dao.domain;
 /**
  * 可排序的对象
  */
-public interface OrderableObject<T> {
+public interface OrderableObject<T extends Comparable> extends Comparable<OrderableObject<T>> {
 
     /**
      * 排序代码
@@ -13,4 +13,17 @@ public interface OrderableObject<T> {
      */
     T getOrderCode();
 
+    @Override
+    default int compareTo(OrderableObject<T> o) {
+
+        if (o == null) {
+            return 1;
+        }
+
+        if (getOrderCode() == null) {
+            return o.getOrderCode() == null ? 0 : -1;
+        }
+
+        return getOrderCode().compareTo(o.getOrderCode());
+    }
 }
