24f9d45b1b7a85c2bcf63ac85e4a9aa140cbdae3
llw <vince2li@gmail.com>
2021-11-11 19:37:26 +0800

    调整模板

--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/aspect/ModuleWebControllerAspect.java
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/aspect/ModuleWebControllerAspect.java
@@ -49,61 +49,10 @@ public class ModuleWebControllerAspect {
 
     final AtomicBoolean enableHttpLog = new AtomicBoolean(false);
 
-
     @PostConstruct
     void init() {
         enableHttpLog.set(enableLog);
     }
-
-    final VariableResolver httpRequestInfoResolver = new VariableResolver() {
-        @Override
-        public <T> ValueHolder<T> resolve(String name, T defaultValue, boolean throwEx, Class<?>... types) throws VariableNotFoundException {
-
-            String value = null;
-
-//            request.getRequestURL() 返回全路径
-//            request.getRequestURI() 返回除去host（域名或者ip）部分的路径
-//            request.getContextPath() 返回工程名部分，如果工程映射为/，此处返回则为空
-//            request.getServletPath() 返回除去host和工程名部分的路径
-
-//            request.getRequestURL() http://localhost:8080/jqueryLearn/resources/request.jsp
-//            request.getRequestURI() /jqueryLearn/resources/request.jsp
-//            request.getContextPath()/jqueryLearn
-//            request.getServletPath()/resources/request.jsp
-
-            if (InjectConsts.IP_ADDR.equalsIgnoreCase(name)) {
-
-                value = IPAddrUtils.try2GetUserRealIPAddr(request);
-
-            } else if (InjectConsts.URL_SERVERNAME.equalsIgnoreCase(name)) {
-
-                value = request.getServerName();
-
-            } else if (InjectConsts.USER_AGENT.equalsIgnoreCase(name)) {
-
-                value = request.getHeader("user-agent");
-
-            } else if (InjectConsts.URL.equalsIgnoreCase(name)) {
-
-                value = request.getRequestURL().toString();
-
-            } else if (InjectConsts.URL_SCHEME.equalsIgnoreCase(name)) {
-
-                value = request.getScheme();
-
-            } else if ("moduleId".equalsIgnoreCase(name)) {
-
-                value = ModuleOption.ID;
-
-            } else {
-                return ValueHolder.notValue();
-            }
-
-            return new ValueHolder()
-                    .setValue(value)
-                    .setHasValue(true);
-        }
-    };
 
     /**
      * 模块包
@@ -158,7 +107,6 @@ public class ModuleWebControllerAspect {
                     .filter(Objects::nonNull)
                     .forEachOrdered(arg -> {
                         variableInjector.injectByVariableResolver(arg
-                                , () -> Arrays.asList(httpRequestInfoResolver)
                                 , () -> variableResolverManager.getVariableResolvers());
                     });
         });
--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleVariableResolverConfigurer.java
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleVariableResolverConfigurer.java
@@ -3,11 +3,19 @@ package ${modulePackageName}.config;
 import static ${modulePackageName}.ModuleOption.*;
 
 import ${modulePackageName}.*;
-import com.levin.commons.service.support.VariableResolverConfigurer;
-import com.levin.commons.service.support.VariableResolverManager;
+import com.levin.commons.service.support.*;
+import com.levin.commons.utils.IPAddrUtils;
+import com.levin.commons.utils.MapUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 @Configuration(PLUGIN_PREFIX + "ModuleVariableResolverConfigurer")
@@ -16,12 +24,95 @@ import org.springframework.context.annotation.Configuration;
 public class ModuleVariableResolverConfigurer
         implements VariableResolverConfigurer {
 
+
     @Override
-    public void config(VariableResolverManager variableResolverManager) {
+    public void config(VariableResolverManager vrm) {
 
         //@todo
         //加入全局的变量解析器，比如用户数据，环境数据等
+        //静态变量
+        vrm.add(MapUtils.putFirst("静态变量", "静态变量").build());
 
+        //http相关的动态变量
+        vrm.add(httpRequestInfoResolver());
+
+        //动态变量
+        vrm.add(new VariableResolver.MapVariableResolver(() -> getContextVars()));
+    }
+
+
+    /**
+     * 获取动态变量
+     * @return
+     */
+    protected List<Map<String, Object>> getContextVars() {
+        //每次请求都会获取的变量
+
+        return Arrays.asList(MapUtils.putFirst(InjectConsts.ORG_ID, "123456789").build());
+
+    }
+
+
+    @Bean
+    HttpRequestInfoResolver httpRequestInfoResolver() {
+        return new HttpRequestInfoResolver();
+    }
+
+    static class HttpRequestInfoResolver implements VariableResolver {
+
+        @Autowired
+        HttpServletRequest request;
+
+        @Autowired
+        HttpServletResponse response;
+
+        @Override
+        public <T> ValueHolder<T> resolve(String name, T defaultValue, boolean throwEx, Class<?>... types) throws VariableNotFoundException {
+
+            String value = null;
+
+//            request.getRequestURL() 返回全路径
+//            request.getRequestURI() 返回除去host（域名或者ip）部分的路径
+//            request.getContextPath() 返回工程名部分，如果工程映射为/，此处返回则为空
+//            request.getServletPath() 返回除去host和工程名部分的路径
+
+//            request.getRequestURL() http://localhost:8080/jqueryLearn/resources/request.jsp
+//            request.getRequestURI() /jqueryLearn/resources/request.jsp
+//            request.getContextPath()/jqueryLearn
+//            request.getServletPath()/resources/request.jsp
+
+            if (InjectConsts.IP_ADDR.equalsIgnoreCase(name)) {
+
+                value = IPAddrUtils.try2GetUserRealIPAddr(request);
+
+            } else if (InjectConsts.URL_SERVERNAME.equalsIgnoreCase(name)) {
+
+                value = request.getServerName();
+
+            } else if (InjectConsts.USER_AGENT.equalsIgnoreCase(name)) {
+
+                value = request.getHeader("user-agent");
+
+            } else if (InjectConsts.URL.equalsIgnoreCase(name)) {
+
+                value = request.getRequestURL().toString();
+
+            } else if (InjectConsts.URL_SCHEME.equalsIgnoreCase(name)) {
+
+                value = request.getScheme();
+
+            } else if ("moduleId".equalsIgnoreCase(name)) {
+
+                value = ModuleOption.ID;
+
+            } else {
+                return ValueHolder.notValue();
+            }
+
+            return new ValueHolder()
+                    .setValue(value)
+                    .setHasValue(true);
+        }
     }
 
 }
