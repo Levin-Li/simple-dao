3b42e63e66b55dc023da63b59a0b5f9f80885317
llw <vince2li@gmail.com>
2022-04-02 01:35:47 +0800

    优化模板

--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleWebControllerAdvice.java
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleWebControllerAdvice.java
@@ -4,6 +4,7 @@ import static ${modulePackageName}.ModuleOption.*;
 import ${modulePackageName}.*;
 
 import com.levin.commons.service.domain.ApiResp;
+import com.levin.commons.service.domain.ServiceResp;
 import com.levin.commons.service.exception.AccessDeniedException;
 import com.levin.commons.service.exception.ServiceException;
 import com.levin.commons.utils.ExceptionUtils;
@@ -25,6 +26,8 @@ import javax.annotation.Resource;
 import javax.persistence.PersistenceException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.net.ConnectException;
+import java.net.SocketException;
 import java.sql.SQLException;
 import java.sql.SQLIntegrityConstraintViolationException;
 
@@ -99,28 +102,13 @@ public class ModuleWebControllerAdvice {
 //        return result;
 //    }
 
-
-    @ExceptionHandler({AccessDeniedException.class,})
-    public ApiResp onAccessDeniedException(Exception e) {
-        return ApiResp.error(2, "访问异常：" + e.getMessage())
-                .setHttpStatusCode(HttpStatus.UNAUTHORIZED.value());
-    }
-
-    @ExceptionHandler({MethodArgumentNotValidException.class,
-            MissingServletRequestParameterException.class})
+    @ExceptionHandler({MethodArgumentNotValidException.class, IllegalArgumentException.class, MissingServletRequestParameterException.class})
     public ApiResp onParameterException(Exception e) {
 
-        log.error("发生数据约束异常," + request.getRequestURL(), e);
+        log.error("请求参数异常," + request.getRequestURL(), e);
 
-        return (ApiResp) ApiResp.error(9, "请求参数异常：" + e.getMessage())
-                .setHttpStatusCode(HttpStatus.BAD_REQUEST.value())
-                .setDetailMsg(ExceptionUtils.getAllCauseInfo(e, " -> "));
-    }
-
-    @ExceptionHandler(ServiceException.class)
-    public ApiResp onServiceException(Exception e) {
-        return (ApiResp) ApiResp.error(10, e.getMessage())
-                .setHttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.BizError.getBaseErrorCode()
+                ,   e.getMessage())
                 .setDetailMsg(ExceptionUtils.getAllCauseInfo(e, " -> "));
     }
 
@@ -132,9 +120,22 @@ public class ModuleWebControllerAdvice {
         boolean used = e.getMessage().contains(" delete ")
                 || e.getMessage().contains(" update ");
 
-        return (ApiResp) ApiResp.error(20, used ? "操作失败，数据已经被使用" : "名称、编码或其它唯一值已经存在")
-                .setHttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.BizError.getBaseErrorCode()
+                , used ? "操作失败，数据已经被使用" : "名称、编码或其它唯一值已经存在")
                 .setDetailMsg(ExceptionUtils.getRootCauseInfo(e));
+    }
+
+    @ExceptionHandler(ServiceException.class)
+    public ApiResp onServiceException(Exception e) {
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.SystemInnerError.getBaseErrorCode()
+                , e.getMessage())
+                .setDetailMsg(ExceptionUtils.getAllCauseInfo(e, " -> "));
+    }
+
+    @ExceptionHandler({AccessDeniedException.class,})
+    public ApiResp onAccessDeniedException(Exception e) {
+        return ApiResp.error(ServiceResp.ErrorType.AuthenticationError.getBaseErrorCode()
+                ,   e.getMessage()) ;
     }
 
 
@@ -142,17 +143,17 @@ public class ModuleWebControllerAdvice {
     public ApiResp onPersistenceException(Exception e) {
 
         Throwable rootCause = ExceptionUtils.getRootCause(e);
-        if (rootCause instanceof ConstraintViolationException || rootCause instanceof SQLIntegrityConstraintViolationException) {
+        if (rootCause instanceof ConstraintViolationException
+                || rootCause instanceof SQLIntegrityConstraintViolationException) {
             return onConstraintViolationException((Exception) rootCause);
         }
 
         log.error("发生数据库操作异常," + request.getRequestURL(), e);
 
-        return (ApiResp) ApiResp.error(20, "数据异常，请稍后重试")
-                .setHttpStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value())
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.SystemInnerError.getBaseErrorCode(),
+                "数据异常，请稍后重试")
                 .setDetailMsg(ExceptionUtils.getRootCauseInfo(e));
     }
-
 
     //    // 这里就是通用的异常处理器了,所有预料之外的Exception异常都由这里处理
     @ExceptionHandler(ServletException.class)
@@ -160,17 +161,26 @@ public class ModuleWebControllerAdvice {
 
         log.error("发生 Web异常:" + request.getRequestURL(), e);
 
-        return (ApiResp) ApiResp.error(1, e.getMessage())
-                .setHttpStatusCode(500)
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.SystemInnerError.getBaseErrorCode()
+                , e.getMessage())
                 .setDetailMsg(ExceptionUtils.getPrintInfo(e));
     }
 
     //    // 这里就是通用的异常处理器了,所有预料之外的Exception异常都由这里处理
     @ExceptionHandler(Exception.class)
     public ApiResp exceptionHandler(Exception e) {
+
         log.error("发生异常:" + request.getRequestURL(), e);
-        return (ApiResp) ApiResp.error(1, e.getMessage())
-                .setHttpStatusCode(500)
+
+        //网络异常
+        if(ExceptionUtils.getCauseByTypes(e,SocketException.class) != null){
+            return (ApiResp) ApiResp.error(ServiceResp.ErrorType.ResourceError.getBaseErrorCode()
+                    , e.getMessage())
+                    .setDetailMsg(ExceptionUtils.getPrintInfo(e));
+        }
+
+        return (ApiResp) ApiResp.error(ServiceResp.ErrorType.UnknownError.getBaseErrorCode()
+                , e.getMessage())
                 .setDetailMsg(ExceptionUtils.getPrintInfo(e));
     }
 }
