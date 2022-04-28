9c4ba6dddf67f598d994335e0e51e4c99984989f
llw <vince2li@gmail.com>
2022-04-28 12:21:45 +0800

    优化模板

--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleSwaggerConfigurer.java
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/config/ModuleSwaggerConfigurer.java
@@ -213,7 +213,9 @@ public class ModuleSwaggerConfigurer implements ModelPropertyBuilderPlugin, WebM
             //  && enumType.getName().startsWith(PACKAGE_NAME)
         ) {
 
-            Enumerated enumerated = pd.getField() != null ? pd.getField().getAnnotation(Enumerated.class) : pd.getGetter().getAnnotation(Enumerated.class);
+            Enumerated enumerated = pd.hasField() ? pd.getField().getAnnotation(Enumerated.class)
+                    : (pd.hasGetter() ? pd.getGetter().getAnnotation(Enumerated.class) : pd.getSetter().getAnnotation(Enumerated.class));
+
             boolean isIndex = enumerated == null || EnumType.ORDINAL.ordinal() == enumerated.value().ordinal();
 
             final List<String> displayValues = Arrays.stream((Enum[]) enumType.getEnumConstants())
