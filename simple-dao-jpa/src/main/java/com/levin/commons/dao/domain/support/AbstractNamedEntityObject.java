a76faef085301a4afff74c630753c2a95d2fbfa7
llw <vince2li@gmail.com>
2022-06-13 12:42:22 +0800

    调整基类

--- simple-dao-jpa/src/main/java/com/levin/commons/dao/domain/support/AbstractNamedEntityObject.java
+++ simple-dao-jpa/src/main/java/com/levin/commons/dao/domain/support/AbstractNamedEntityObject.java
@@ -3,6 +3,8 @@ package com.levin.commons.dao.domain.support;
 import com.levin.commons.dao.annotation.Contains;
 import com.levin.commons.dao.domain.NamedEntityObject;
 import com.levin.commons.service.domain.Desc;
+import com.levin.commons.service.domain.InjectVar;
+import com.levin.commons.service.support.PrimitiveArrayJsonConverter;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -10,6 +12,7 @@ import lombok.experimental.FieldNameConstants;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import java.util.List;
 
 @MappedSuperclass
 @Data
@@ -30,10 +33,18 @@ public abstract class AbstractNamedEntityObject
     @Contains
     protected String name;
 
-    @Schema(description = "拼音，格式：全拼(简拼)")
+    @Schema(description = "拼音，格式Json数组：[全拼,简拼]")
     @Column(length = 128)
-    @Contains
+    @InjectVar(domain = "dao", expectBaseType = List.class, expectGenericTypes = {String.class}, converter = PrimitiveArrayJsonConverter.class, isRequired = "false")
     protected String pinyinName;
+
+//    查询对象的定义方式
+//    @Schema(description = "拼音列表")
+//    @OR(autoClose = true)
+//    @InjectVar(domain = "dao", converter = JsonStrLikeConverter.class)
+//    @Contains
+//    private List<String> pinyinNameList;
+
 
     @Override
     public String toString() {
