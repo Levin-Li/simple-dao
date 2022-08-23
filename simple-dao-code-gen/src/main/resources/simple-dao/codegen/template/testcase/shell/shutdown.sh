083948fc5c6f9a6785379b040e13d6e60a8fa4ff
Levin-Li <vince2li@gmail.com>
2022-08-23 12:03:42 +0800

    add id gen

--- simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/testcase/shell/shutdown.sh
+++ simple-dao-code-gen/src/main/resources/simple-dao/codegen/template/testcase/shell/shutdown.sh
@@ -12,7 +12,7 @@ shellDir=`pwd`
 #获取进程ID
 pids=`ps -ef | grep java | grep "$shellDir" | awk '{print $2}'`
 
-if [ -z $pids ]; then
+if [ -z "${pids}" ]; then
 
    echo "[$shellDir/$0] program already stop, nothing to do."
 
@@ -32,7 +32,7 @@ else
 
       pids=`ps -ef | grep java | grep "$shellDir" | awk '{print $2}'`
 
-      if [ -z $pids ]; then
+      if [ -z "${pids}" ]; then
          echo "program ${tempPid} stopped."
          exit
       fi
@@ -45,7 +45,7 @@ fi
 
 pids=`ps -ef | grep java | grep "$shellDir" | awk '{print $2}'`
 
-if [ -n "$pids" ]; then
+if [ -n "${pids}" ]; then
    echo "***Warning*** kill -9 $pids ..."
    kill -9 $pids
    sleep 1
