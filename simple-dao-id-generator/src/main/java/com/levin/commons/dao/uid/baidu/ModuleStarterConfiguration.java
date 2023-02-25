a11085f6ee3c5faa5f9fbd4be15fd03b6a36a23f
Levin-Li <vince2li@gmail.com>
2023-02-26 00:47:45 +0800

    优化代码生成

--- simple-dao-id-generator/src/main/java/com/levin/commons/dao/uid/baidu/ModuleStarterConfiguration.java
+++ simple-dao-id-generator/src/main/java/com/levin/commons/dao/uid/baidu/ModuleStarterConfiguration.java
@@ -1,6 +1,7 @@
 package com.levin.commons.dao.uid.baidu;
 
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.*;
 import org.springframework.core.env.*;
 
@@ -10,6 +11,8 @@ import static com.levin.commons.dao.uid.baidu.ModuleOption.*;
 
 @Role(BeanDefinition.ROLE_SUPPORT)
 @Configuration(PLUGIN_PREFIX + "ModuleStarterConfiguration")
+
+@EntityScan({ PACKAGE_NAME+".worker.entity"})
 @ComponentScan({PACKAGE_NAME})
 public class ModuleStarterConfiguration {
 
