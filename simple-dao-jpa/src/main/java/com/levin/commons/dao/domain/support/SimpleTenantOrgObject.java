87817bbc1d39612f90be987dfc5b87588d3e70eb
Levin-Li <vince2li@gmail.com>
2023-06-28 13:23:45 +0800

    优化gitpack打包策略

--- simple-dao-jpa/src/main/java/com/levin/commons/dao/domain/support/SimpleTenantOrgObject.java
+++ simple-dao-jpa/src/main/java/com/levin/commons/dao/domain/support/SimpleTenantOrgObject.java
@@ -1,6 +1,7 @@
 package com.levin.commons.dao.domain.support;
 
 import com.levin.commons.dao.domain.MultiTenantObject;
+import com.levin.commons.dao.domain.OrganizedObject;
 import com.levin.commons.service.domain.InjectVar;
 import com.levin.commons.service.support.InjectConsts;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,6 +11,9 @@ import lombok.experimental.FieldNameConstants;
 
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import java.util.Date;
 
 //1、lobmok get set
 @Data
@@ -24,21 +28,25 @@ import javax.persistence.MappedSuperclass;
 @FieldNameConstants
 
 //5、必须注解业务名称
-@Schema(title = "抽象租户实体")
+@Schema(title = "抽象租户组织实体")
 
 @MappedSuperclass
-@Deprecated
-public abstract class AbstractNamedMultiTenantObject
-        extends AbstractNamedEntityObject
-        implements MultiTenantObject {
+public abstract class SimpleTenantOrgObject
+        implements MultiTenantObject, OrganizedObject {
 
     @Schema(title = "租户ID")
     @Column(length = 128)
     @InjectVar(InjectConsts.TENANT_ID)
     protected String tenantId;
 
-    @Schema(title = "系统域")
+    @Schema(title = "组织机构ID")
     @Column(length = 128)
-    protected String domain;
+    @InjectVar(InjectConsts.ORG_ID)
+    protected String orgId;
+
+    @Schema(title = "创建时间")
+    @Column(nullable = false)
+    @Temporal(value = TemporalType.TIMESTAMP)
+    protected Date createTime;
 
 }
