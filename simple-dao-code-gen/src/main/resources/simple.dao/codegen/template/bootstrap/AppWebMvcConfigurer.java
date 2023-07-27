a3a64b4c2f3467eddef1546405fe94a910afd369
Levin-Li <vince2li@gmail.com>
2023-07-27 09:27:21 +0800

    优化代码模板

--- simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/bootstrap/AppWebMvcConfigurer.java
+++ simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/bootstrap/AppWebMvcConfigurer.java
@@ -9,8 +9,11 @@ import org.springframework.core.convert.converter.ConverterFactory;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.beans.factory.annotation.*;
+import com.levin.commons.format.DefaultDateFormat;
 
 import javax.annotation.PostConstruct;
+
+import java.util.Date;
 
 import static ${modulePackageName}.ModuleOption.*;
 
@@ -23,49 +26,26 @@ import static ${modulePackageName}.ModuleOption.*;
 @Configuration(PLUGIN_PREFIX + "AppWebMvcConfigurer")
 @ConditionalOnProperty(prefix = PLUGIN_PREFIX, name = "AppWebMvcConfigurer", matchIfMissing = true)
 public class AppWebMvcConfigurer implements WebMvcConfigurer {
-
-//    @Autowired
-//    RbacService rbacService;
-//
-//    @Autowired
-//    AuthService authService;
-//
-//    @Autowired
-//    BizTenantService bizTenantService;
-
     @PostConstruct
     void init() {
         log.info("init...");
     }
-
-//    static class String2EnumCF implements ConverterFactory<String, Enum> {
-//        @Override
-//        public <T extends Enum> Converter<String, T> getConverter(Class<T> targetType) {
-//            return name -> (T) EnumDesc.parse(targetType, name);
-//        }
-//    }
-//
-//    static class Number2EnumCF implements ConverterFactory<Number, Enum> {
-//        @Override
-//        public <T extends Enum> Converter<Number, T> getConverter(Class<T> targetType) {
-//            return code -> (T) EnumDesc.parse(targetType, code.intValue());
-//        }
-//    }
-
     @Override
     public void addFormatters(FormatterRegistry registry) {
 
         //移除默认的转换器
-        registry.removeConvertible(String.class, Enum.class);
-        registry.removeConvertible(int.class, Enum.class);
-        registry.removeConvertible(Integer.class, Enum.class);
-        registry.removeConvertible(Number.class, Enum.class);
+//        registry.removeConvertible(String.class, Enum.class);
+//        registry.removeConvertible(int.class, Enum.class);
+//        registry.removeConvertible(Integer.class, Enum.class);
+//        registry.removeConvertible(Number.class, Enum.class);
+//
+//        registry.addConverterFactory(EnumDesc.string2EnumFactory);
+//        registry.addConverterFactory(EnumDesc.number2EnumFactory);
 
-//        registry.addConverterFactory(new String2EnumCF());
-//        registry.addConverterFactory(new Number2EnumCF());
+        registry.removeConvertible(String.class, Date.class);
+        registry.removeConvertible(Date.class, String.class);
 
-        registry.addConverterFactory(EnumDesc.string2EnumFactory);
-        registry.addConverterFactory(EnumDesc.number2EnumFactory);
+        registry.addFormatter(new DefaultDateFormat());
 
     }
 
