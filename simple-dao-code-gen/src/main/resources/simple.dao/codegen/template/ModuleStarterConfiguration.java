7b5bfc85123943d82fed2b866b5c03d0c7feee6f
Levin-Li <vince2li@gmail.com>
2023-08-17 00:40:41 +0800

    优化代码生成

--- simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/ModuleStarterConfiguration.java
+++ simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/ModuleStarterConfiguration.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * 模块需要自举加载的内容都需要配置在该类中
  * @author Auto gen by simple-dao-codegen, @time: ${.now}, 代码生成哈希校验码：[]，请不要修改和删除此行内容。
- * 
+ *
  */
 @Configuration(PLUGIN_PREFIX + "${className}")
 @Slf4j
@@ -36,7 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 @EntityScan({PACKAGE_NAME})
 
 // Spring 扫描
-@ComponentScan(value = {PACKAGE_NAME})
+//@ComponentScan({PACKAGE_NAME})
 
 // 自定义注解接口 扫描
 @ProxyBeanScan(basePackages = {PACKAGE_NAME} , scanType = EntityRepository.class , factoryBeanClass = RepositoryFactoryBean.class)
@@ -44,8 +44,9 @@ import org.springframework.web.bind.annotation.RestController;
 // FeignClients 扫描
 @EnableFeignClients({PACKAGE_NAME})
 
-// Dubbo 扫描
-@DubboComponentScan(basePackages = {PACKAGE_NAME})
+
+// Dubbo 扫描，根据现有的 Dubbo 3.1.x 版本的机制， DubboComponentScan 会先自动先扫描 Spring 的注解, 所以 @ComponentScan 可以注释。
+@DubboComponentScan({PACKAGE_NAME})
 
 public class ModuleStarterConfiguration {
 
