771005279d3169d21292e7f8eca00375f80ee6f2
lilw <vince2li@gmail.com>
2024-04-17 10:58:41 +0800

    增加查询对象的列选择方法

--- simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/entity/TestOrg.java
+++ simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/entity/TestOrg.java
@@ -20,6 +20,9 @@ import org.hibernate.annotations.Type;
 
 /**
  * 示例代码
+ *
+ * 主要用于实体模型的定义参考
+ *
  * @author Auto gen by simple-dao-codegen, @time: ${.now}, 代码生成哈希校验码：[]，请不要修改和删除此行内容。
  *
  */
@@ -61,22 +64,16 @@ import org.hibernate.annotations.Type;
         }
 )
 
-
-//逻辑删除的例子
-//@EntityOption(disableActions = EntityOption.Action.Delete,logicalDeleteFieldName = "deleted",logicalDeleteValue = "true")
-
-//@EntityListener(xxListener.class)
-
+//JPA 继承配置
 //@Inheritance(strategy = InheritanceType.JOINED)//定义实体类的继承策略，这里表示使用Joined Table的继承策略，子类和父类分别映射到不同的数据库表
-//@DiscriminatorColumn(name = E_User.type)//DiscriminatorColumn注解指定了区分列的名称
-// 子类中需要配置注解 @DiscriminatorValue("Legal")
+//@DiscriminatorColumn(name = E_${entityName}.exType)//DiscriminatorColumn注解指定了区分列的名称
 
 //DiscriminatorColumn 字段例子
 //@Column(length = 64, nullable = false, insertable = false, updatable = false)
-//String type;
+//String exType;
 
-//默认
-//@DiscriminatorValue("Default")
+//当前类和子类都要配置鉴别值
+//@DiscriminatorValue(E_${entityName}.SIMPLE_CLASS_NAME)
 
 //关于 JPA 继承模型
 //@DiscriminatorColumn
@@ -85,14 +82,28 @@ import org.hibernate.annotations.Type;
 //@Inheritance( strategy = InheritanceType.TABLE_PER_CLASS )
 //@MappedSuperclass告诉JPA提供者包含基类的持久性属性，就好像它们是由扩展用@MappedSuperclass注解的超类的子类所声明的@MappedSuperclass 。
 //但是，inheritance仅在OOP世界中是可见的，因为从数据库的angular度来看，没有任何基类的迹象。 只有子类实体将有一个关联的映射表。
-//@Inheritance注释是为了实现数据库表结构中的OOPinheritance模型。 更多的，你可以查询用@Inheritance注解的基类，但是你不能用@MappedSuperclass注解的基类。
+//@Inheritance注释是为了实现数据库表结构中的OOP inheritance模型。 更多的，你可以查询用@Inheritance注解的基类，但是你不能用@MappedSuperclass注解的基类。
 //现在，您要使用@Inheritance JPA注释的原因是要实施像“战略模式”这样的行为驱动模式 。另一方面， @MappedSuperclass只是一种重用基本属性，关联，甚至是使用公共基类的实体@Id方法。
 //不过，使用@Embeddabletypes可以达到几乎相同的目标。 唯一的区别是你不能重复@Embeddable的@Id定义，但你可以用@MappedSuperclass 。
 
 //实体模型的类别
 @EntityCategory(EntityOpConst.BIZ_TYPE_NAME)
+//逻辑删除的例子
+//@EntityOption(disableActions = EntityOption.Action.Delete,logicalDeleteFieldName = "deleted",logicalDeleteValue = "true")
 
-//@Cacheable(false) //JPA缓存
+//JPA 监听器
+//@EntityListener(${entityName}Listener.class)
+
+//JPA二级缓存
+//@Cacheable(false) //禁止或启用缓存，默认为true，在实体类对应的服务类${entityName}Service中会启用或是禁用缓存
+
+//对父类的属性定义进行覆盖，通常是对数据库相关的（如字段长度，是否允许空等）属性重新定义
+//@AttributeOverride(name = E_TestOrg.tenantId, column = @Column(nullable = false, length = 128))
+@AttributeOverride(name = E_TestOrg.name, column = @Column(nullable = false, length = 128))
+
+//关联属性重新定义
+//@AssociationOverride(name = E_${entityName}.tenantId)
+//@AssociationOverride(name = E_${entityName}.orgId)
 
 public class TestOrg
         extends AbstractTreeObject<String, TestOrg>
