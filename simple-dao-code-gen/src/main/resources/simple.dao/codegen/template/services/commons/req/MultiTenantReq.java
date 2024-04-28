08364a4924d882d604de7cbcf2eb0250219a2f07
lilw <vince2li@gmail.com>
2024-04-28 10:32:56 +0800

    优化pom依赖

--- simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/services/commons/req/MultiTenantReq.java
+++ simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/services/commons/req/MultiTenantReq.java
@@ -38,8 +38,10 @@ public class MultiTenantReq<T extends MultiTenantReq<T>>
             , isOverride = InjectVar.SPEL_PREFIX + NOT_SUPER_ADMIN // 如果不是超级管理员, 那么覆盖必须的
             , isRequired = InjectVar.SPEL_PREFIX + NOT_SUPER_ADMIN // 如果不是超级管理员，那么值是必须的
     )
+    @OrderBy(condition = "#_isQuery && isSuperAdmin && #isEmpty(#_fieldVal)", type = OrderBy.Type.Asc
+            , order = Integer.MIN_VALUE, scope = OrderBy.Scope.OnlyForNotGroupBy, desc = "本排序规则是把租户ID为NULL的排在前面")
     @OrderBy(condition = "#_isQuery && !isSuperAdmin && #isNotEmpty(#_fieldVal) && isContainsPublicData() && !isTenantShared()",
-            order = Integer.MIN_VALUE, scope = OrderBy.Scope.OnlyForNotGroupBy, desc = "本排序规则是本租户的数据排第一个，通常用于只取一个数据时，先取自己租户的数据")
+            order = Integer.MIN_VALUE, scope = OrderBy.Scope.OnlyForNotGroupBy, desc = "本排序规则是把租户ID不为NULL的排在前面")
     @OR(autoClose = true)
     @Eq
     @IsNull(condition = "#_isQuery && !isSuperAdmin && isContainsPublicData() ", desc = "查询结果包含公共数据(tenantId为NULL的数据)")
