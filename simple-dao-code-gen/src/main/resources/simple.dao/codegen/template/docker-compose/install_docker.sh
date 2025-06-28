504a4b2865b6fb515c719ce95fe148ce63230ca9
lilw <vince2li@gmail.com>
2025-06-28 09:55:20 +0800

    增加docker支持文件

---
+++ simple-dao-code-gen/src/main/resources/simple.dao/codegen/template/docker-compose/install_docker.sh
@@ -0,0 +1,9 @@
+
+sudo yum install -y docker
+
+sudo systemctl start docker
+sudo systemctl enable docker
+
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
